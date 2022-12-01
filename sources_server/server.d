634bf399c214bc030c5b1b75eb75f3f632fc2c47
Giuliano Dominici <giuliandominici@gmail.com>
2022-12-01 19:54:48 +0100

    Done

--- sources_server/server.d
+++
@@ -1,8 +0,0 @@
-sources_server/server.o: sources_server/server.c \
-  sources_server/../libft/includes/libft.h \
-  libft/includes/./libft_checks.h libft/includes/libft.h \
-  libft/includes/./libft_converse.h libft/includes/./libftprintf.h \
-  libft/includes/./libft_list.h libft/includes/./libft_memory.h \
-  libft/includes/./libft_puts.h libft/includes/./libft_strings.h \
-  libft/includes/./get_next_line_bonus.h \
-  sources_server/../includes/minitalk.h
